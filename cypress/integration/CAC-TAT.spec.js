3064606dd501b8c3443ff8d09e579052f71c406f
Paulo Sandim <pauloedusandim@gmail.com>
2022-07-21 21:09:29 -0300

    exercicios aulas 3 e 4

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,9 +1,111 @@
 /// <reference types="Cypress" />
 
-describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+const faker = require('faker')
+
+describe('Central de Atendimento ao Cliente TAT', () => {
+    const usuario = {}
+
+    beforeEach(() => {
         cy.visit('./src/index.html')
-        
-        cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
+
+        usuario.nome = faker.name.firstName()
+        usuario.sobrenome = faker.name.lastName()
+        usuario.email = faker.internet.email()
+        usuario.mensagem = faker.lorem.lines()
     })
+
+    it('verifica o título da aplicação', () => {      
+        cy.title()
+        .should('be.equal','Central de Atendimento ao Cliente TAT')
+    })
+
+    it('preenche os campos obrigatórios e envia o formulário', () => {
+        const longText = 'experimente digitar um texto longo na área de texto, passando como segundo argumento do comando'
+        
+        cy.get('#firstName').type('Paulo')
+        cy.get('#lastName').type('Sandim')
+        cy.get('#email').type('pauloedusandim@gmail.com')
+        cy.get('#open-text-area').type(longText, { delay: 0 })
+        cy.contains('button', 'Enviar').click()
+
+        cy.get('.success > strong').should('be.visible')
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+        cy.get('#email').type('teste')
+        cy.contains('button', 'Enviar').click()
+
+        cy.get('.error> strong').should('be.visible')
+    })
+
+    it('verifica se um valor não-numérico for digitado no campo telefone', () => {
+        cy.get('#phone').type('abc').should('have.value', '')
+    })
+      
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+        cy.get('#phone-checkbox').click()
+        cy.contains('button', 'Enviar').click()
+        
+        cy.get('.error> strong').should('be.visible')
+    })
+
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+        cy.get('#firstName').type('Paulo')
+            .should('have.value', 'Paulo')
+            .clear().should('have.value', '')
+        
+        cy.get('#lastName').type('Sandim')
+            .should('have.value', 'Sandim')
+            .clear().should('have.value', '')
+        
+        cy.get('#email').type('pauloedusandim@gmail.com')
+            .should('have.value', 'pauloedusandim@gmail.com')
+            .clear().should('have.value', '')
+        
+        cy.get('#phone').type('67999012020')
+            .should('have.value', '67999012020')
+            .clear().should('have.value', '')
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+        cy.contains('button', 'Enviar').click()
+        
+        cy.get('.error> strong').should('be.visible')
+    })
+
+    it('envia o formuário com sucesso usando um comando customizado', () => {
+        cy.fillMandatoryFieldsAndSubmit(usuario)
+
+        cy.get('.success > strong').should('be.visible')
+    })
+
+    it('seleciona um produto (YouTube) por seu texto', () => {
+        cy.get('#product').select('YouTube')
+        .should('have.value', 'youtube')
+    })
+
+    it('seleciona um produto (Mentoria) por seu valor (value)', () => {
+        cy.get('#product').select('mentoria')
+        .should('have.value', 'mentoria')
+    })
+
+    it('seleciona um produto (Blog) por seu índice', () => {
+        cy.get('#product').select(1)
+        .should('have.value', 'blog')
+    })
+
+    it('marca o tipo de atendimento "Feedback"', () => {
+        cy.get('input[type="radio"]').check('feedback')
+        .should('have.value', 'feedback')
+    })
+
+    it.only('marca cada tipo de atendimento', () => {
+        cy.get('input[type="radio"]')
+        .should('have.length', 3)
+        .each(($radio) => {
+            cy.wrap($radio).check()
+            cy.wrap($radio).should('be.checked')
+        }) 
+    });
+
 })
